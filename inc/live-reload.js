de0db4ca28cf6bd8c82c97efcab9bb8e173eadd8
juzer09 <juzer@quickshiftdigital.com>
2022-05-26 16:03:11 +0530

    v0.2.0

---
+++ inc/live-reload.js
@@ -0,0 +1,15 @@
+var connectLiveReload = require("connect-livereload");
+
+var livereload = require("livereload");
+var connectLiveReload = require("connect-livereload");
+
+const liveReloadServer = livereload.createServer();
+liveReloadServer.server.once("connection", () => {
+    setTimeout(() => {
+        liveReloadServer.refresh("/");
+    }, 100);
+});
+
+app.use(connectLiveReload());
+
+exports.liveReloadServer = liveReloadServer;
